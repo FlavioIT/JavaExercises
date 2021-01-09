f95d8a8d77bbdd01826e5809151c10a969b823b2
FlavioIT <flavio.cintra99@gmail.com>
2021-01-09 14:47:01 -0300

    Abstract Demo

--- AbstractDemo/src/AbstractDemo.java
+++ AbstractDemo/src/AbstractDemo.java
@@ -1,5 +1,25 @@
-abstract class AAbstractDemo {
-    public static void main(String[] args) throws Exception {
-        
+abstract class AbstractDemo {
+    public int a;
+    public AbstractDemo(){
+        a = 10;
     }
-}
+
+    abstract public void set(int a);
+    abstract public void get();
+    }
+
+    class Demo extends AbstractDemo{
+        public void set(int a){
+            this.a = a;
+        }
+        final public void get(){
+            System.out.println("a = " + a);
+        
+        }
+        public static void main(String[] args){
+            Demo obj = new Demo();
+            obj.set(20);
+            obj.get();
+        }
+    }
+
